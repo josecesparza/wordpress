ab2bf83cca16482d520f5615488ae8342c8c81d7
jose <josecarlosesparza@gmail.com>
2019-11-14 17:37:55 +0100

    Include bootstrap CSS and JS files

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+//  Include the Bootstrap CSS and Javascript files
+// Including the Bootstrap CSS files
+function bootstrapstarter_enqueue_styles() {
+    wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+    $dependencies = array('bootstrap');
+    wp_enqueue_style( 'bootstrapstarter-style', get_stylesheet_uri(), $dependencies ); 
+}
+
+// Including the Bootstrap JS files
+function bootstrapstarter_enqueue_scripts() {
+    $dependencies = array('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'bootstrapstarter_enqueue_scripts' );
+
+?>
