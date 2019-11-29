d47810ce6f96bac42deff6ab60c0a47b262ce4eb
jose <josecarlosesparza@gmail.com>
2019-11-29 20:05:31 +0100

    Footer changes

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
-<footer class="blog-footer">
-    <p>Webshop template built for <a href="https://www.ucn.dk/english">UCN</a>.</p>
+<footer class="page-footer font-small">
+    <div class="footer-copyright text-center py-3">
+        Webshop template built for <a href="https://www.ucn.dk/english">UCN</a>.
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
+
 </html>
